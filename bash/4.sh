34e73cb6296601982ab84ee17e7babb365e9e648
Nika <chnsv3003@mail.ru>
2025-03-15 16:02:39 +0000

    bash

--- bash/4.sh
+++ bash/4.sh
@@ -1,2 +1,26 @@
 #!/bin/bash
 
+if [ $# -eq 0 ]; then
+    echo "Unknown planet"
+    exit 1
+fi
+
+declare -A planets=(
+    ["Mercury"]=0
+    ["Venus"]=0
+    ["Earth"]=1
+    ["Mars"]=2
+    ["Jupiter"]=98
+    ["Saturn"]=83
+    ["Uranus"]=27
+    ["Neptune"]=14
+)
+
+planet="$1"
+
+if [[ -z "${planets[$planet]}" ]]; then
+    echo "Неизвестная планета"
+else
+    # Вывод количества спутников
+    echo "${planets[$planet]}"
+fi
